c0a2951d84bb32159c1a15ecc2ef88f58bf4cbd9
Affeeq <affeeqabedeenismail@gmail.com>
2019-12-11 16:13:57 +0000

    add flash messages at login route

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,9 @@ router.get("/login", function(req,res) {
 router.post("/login", passport.authenticate("local", 
 	{
 		successRedirect: "/campgrounds",
-		failureRedirect: "/login"
+		failureRedirect: "/login",
+		failureFlash: true,
+		successFlash: "Welcome back!"
 	}), function(req,res) {
 });
 
